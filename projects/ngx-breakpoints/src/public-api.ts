3d01cf932eae68672b482e89975aae399b830940
Florian Diesel <florian.diesel@posteo.de>
2023-03-14 23:43:09 +0100

    release

--- projects/ngx-breakpoints/src/public-api.ts
+++ projects/ngx-breakpoints/src/public-api.ts
@@ -2,13 +2,15 @@
  * Public API Surface of ngx-breakpoints
  */
 
-export * from "./lib/desktop/desktop-attr-list.directive";
-export * from "./lib/desktop/desktop-class.directive";
-export * from "./lib/desktop/desktop-max-width.directive";
-export * from "./lib/desktop/desktop-only.directive";
-export * from "./lib/dynamic/greater-only.directive";
-export * from "./lib/dynamic/lower-only.directive";
-export * from "./lib/mobile/mobile-attr-list.directive";
-export * from "./lib/mobile/mobile-class.directive";
-export * from "./lib/mobile/mobile-only.directive";
-export * from "./lib/ngx-breakpoints.module";
+export * from './lib/breakpoint.service';
+export * from './lib/breakpoint.types';
+export * from './lib/desktop/desktop-attr-list.directive';
+export * from './lib/desktop/desktop-class.directive';
+export * from './lib/desktop/desktop-max-width.directive';
+export * from './lib/desktop/desktop-only.directive';
+export * from './lib/dynamic/greater-only.directive';
+export * from './lib/dynamic/lower-only.directive';
+export * from './lib/mobile/mobile-attr-list.directive';
+export * from './lib/mobile/mobile-class.directive';
+export * from './lib/mobile/mobile-only.directive';
+export * from './lib/ngx-breakpoints.module';
